4391b06895063afe8e59f0dc19a260a429d9d431
white-parrot <everydaycodestory@gmail.com>
2024-11-20 12:36:13 +0530

    LRNNG-HSTL: Base Project added for Product and Coupon API

---
+++ apis/coupon-api/src/test/java/org/turtle/practice/api/coupon/CouponApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.turtle.practice.api.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ apis/product-api/src/test/java/org/turtle/practice/product_api/ProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.turtle.practice.product_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
